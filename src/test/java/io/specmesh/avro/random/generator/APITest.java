11871bb49789a1f396b551ca8e734aed0fe73a39
navery-max <navery@marketaxess.com>
2024-02-01 16:21:09 +0000

    added API with Consumer callback

---
+++ src/test/java/io/specmesh/avro/random/generator/APITest.java
@@ -0,0 +1,74 @@
+package io.specmesh.avro.random.generator;
+
+import org.apache.avro.generic.GenericRecord;
+import org.junit.Test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.BiConsumer;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+
+public class APITest {
+
+
+    @Test
+    public void shouldGenerateAndCallConsumer() {
+
+        int count1 = 10;
+        final var api = new API(count1, "key", schema);
+
+        final var counter = new AtomicInteger();
+
+        api.run(new BiConsumer<Object, GenericRecord>() {
+            @Override
+            public void accept(Object key, GenericRecord genericRecord) {
+                counter.incrementAndGet();
+                System.out.println(key);
+            }
+        });
+
+        assertThat(counter.get(), is(count1));
+    }
+
+    private static String schema = "{\n" +
+            "  \"type\": \"record\",\n" +
+            "  \"name\": \"simple_schema\",\n" +
+            "  \"namespace\": \"io.specmesh.avro.random.generator\",\n" +
+            "  \"fields\": [\n" +
+            "    {\n" +
+            "      \"name\": \"key\",\n" +
+            "        \"type\": {\n" +
+            "          \"type\": \"string\",\n" +
+            "          \"arg.properties\": {\n" +
+            "            \"regex\": \"[a-zA-Z]*\",\n" +
+            "            \"length\": 10\n" +
+            "          }\n" +
+            "        }\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"name\": \"number_length_property\",\n" +
+            "      \"type\":\n" +
+            "      {\n" +
+            "        \"type\": \"string\",\n" +
+            "        \"arg.properties\": {\n" +
+            "          \"regex\": \"[a-zA-Z]*\",\n" +
+            "          \"length\": 10\n" +
+            "        }\n" +
+            "      }\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"name\": \"MY_STRING\",\n" +
+            "      \"type\": {\n" +
+            "        \"type\": \"string\",\n" +
+            "        \"arg.properties\": {\n" +
+            "          \"regex\": \"[a-zA-Z0-9]{16}\"\n" +
+            "        }\n" +
+            "      },\n" +
+            "      \"default\": \"\"\n" +
+            "    }\n" +
+            "  ]\n" +
+            "}\n";
+
+}
